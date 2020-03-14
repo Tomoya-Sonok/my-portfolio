cd70d226e3478ea15f1a83848d521fa64d93e415
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-15 03:07:28 +0900

    created my-portfolio and built its scaffold

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About Me</h1>
+      <p>I currently study and practice Gatsby.js</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/works.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const WorksPage = () => {
+  return (
+    <Layout>
+      <h1>Works</h1>
+      <p>My works will show up here later on.</p>
+    </Layout>
+  )
+}
+
+export default WorksPage
