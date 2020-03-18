ec65b591a8190672eb64642b38ebc13db2defbf4
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-19 00:41:59 +0900

    styled base color of layout-page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import footerStyles from '../components/footer.module.scss'
 
 const Footer = () =>{
   return (
-    <footer>
-      <p>Created by Tomoya Sonokui, <span 
-      role = "img"
-      aria-label = "">©️</span>2020</p>
+    <footer className={footerStyles.footer}>
+      <p className={footerStyles.content}>
+        Created by Tomoya Sonokui, 
+        <span 
+          role = "img"
+          aria-label = "emoji">
+          ©️
+        </span>
+        2020
+      </p>
     </footer>
   )
 }
