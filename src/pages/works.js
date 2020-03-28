de5797264fb7a2b1fb8b098268572ce9704e38f1
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-03-28 23:04:14 +0900

    added some styles and OOTD to portfolio-box

--- src/pages/works.js
+++ src/pages/works.js
@@ -10,16 +10,21 @@ const worksPage = () => {
       <div>
         <ul className={worksStyles.portfoliobox}>
           <li className={worksStyles.portfolio}>
+            <h3>OOTD</h3>
+            <a href="http://18.178.222.209:10308/" target="__blank">
+              <img src="/ootd-toppage.jpg" alt="portfolio" />
+            </a>
+            <p>開発ツール: Ruby on Rails/jQuery/..</p>
+            <p>撮影したコーディネートを視覚的に整理・分類することで、服選びをより簡単にしてくれるアプリです。</p>
+          </li>
+          <li className={worksStyles.portfolio}>
+            <h3>（作品名）</h3>
             <img src="/300_180.jpg" alt="portfolio" />
             <p>使用言語: </p>
             <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
           </li>
           <li className={worksStyles.portfolio}>
-            <img src="/300_180.jpg" alt="portfolio" />
-            <p>使用言語: </p>
-            <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-          </li>
-          <li className={worksStyles.portfolio}>
+            <h3>（作品名）</h3>
             <img src="/300_180.jpg" alt="portfolio" />
             <p>使用言語: </p>
             <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
