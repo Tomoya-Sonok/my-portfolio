3064ee7cbaa74dfb870aa4e16882dc0bbe67d5f6
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-04-02 20:50:09 +0900

    updated contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,27 +12,27 @@ const contactPage = () => {
         <div className={contactStyles.topForm}>
           <div className={contactStyles.innerForm}>
             <div className={contactStyles.label}>name</div>
-            <input type="text" placeholder="Yamada Taro" />
+            <input type="text" name="name" placeholder="Yamada Taro" />
           </div>
           <div className={contactStyles.innerForm}>
             <div className={contactStyles.label}>email</div>
-            <input type="text" placeholder="example@gmail.com" />
+            <input type="text" name="email" placeholder="example@gmail.com" />
           </div>
           <div className={contactStyles.innerForm}>
             <div className={contactStyles.label}>phone</div>
-            <input type="text" placeholder="1234567890" />
+            <input type="text" name="tel" placeholder="1234567890" />
           </div>
         </div>
         <div className={contactStyles.middleForm}>
           <div className={contactStyles.innerForm}>
             <div className={contactStyles.label}>subject</div>
-            <input type="text" placeholder="subject" />
+            <input type="text" name="subject" placeholder="subject" />
           </div>
         </div>
         <div className={contactStyles.bottomForm}>
           <div className={contactStyles.innerForm}>
             <div className={contactStyles.label}>your message</div>
-            <textarea placeholder="your message" />
+            <textarea name="message" placeholder="your message" />
           </div>
         </div>
         <button type="submit" className={contactStyles.btn}>send form</button>
